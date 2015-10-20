35b7117d39cfdb1a715dfa1144e42d6f72e5730c
Piotr Bartela <portfel666@gmail.com>
2015-10-20 23:28:12 +0200

    Cleaned up.

--- src/pl/wallet616/Main/Main.java
+++ src/pl/wallet616/Main/Main.java
@@ -6,12 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DataRead.loadUsers();
-		
-		Log.log(usersList[0][0]);
-		DataSave.changeData("jkhgfbsfsgdfgsdfgs", "userKey", "Mentorkie");
-		Log.log(usersList[0][0]);
-		
-		DataSave.addData("521das221asd", "userKey");
 	}
 
 }
