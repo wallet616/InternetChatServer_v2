590d5d001766b432c638312dd53ff18b58ce1b9c
Piotr Bartela <portfel666@gmail.com>
2015-10-31 00:48:27 +0100

    Messages input cleaning

--- src/pl/wallet616/Main/CommandHandler.java
+++ src/pl/wallet616/Main/CommandHandler.java
@@ -10,22 +10,25 @@ public class CommandHandler extends Main{
 		boolean userFound = false;
 		int id = 0;
 		
-		breakpoint:
-        	while (id < slots) {
+		if (checkIfNotNull(message[0])) {
+			while (id < slots) {
 				if (usersList[id][0] != null && usersList[id][0].equals(message[0])) {
 					userFound = true;
-					break breakpoint;
+					break;
 				}
 				id++;
-		    }
+			}
+		}
 		
 		if (userFound) {
 			if (message[1].equals("arch")) {
 				usersList[id][2] = String.valueOf(System.currentTimeMillis());
 				String returnString = "";
-				for (int i = archivemem - 1; i >= 0; i--) {
-					if (archive[i][0] != null && Integer.parseInt(message[2]) < Integer.parseInt(archive[i][0])) {
-						returnString += ".ws1" + archive[i][0] + ".ws2" + archive[i][1] + ".ws2" + archive[i][2] + ".ws2" + archive[i][3];
+				if (checkIfNumber(message[2])) {
+					for (int i = archivemem - 1; i >= 0; i--) {
+						if (archive[i][0] != null && Integer.parseInt(message[2]) < Integer.parseInt(archive[i][0])) {
+							returnString += ".ws1" + archive[i][0] + ".ws2" + archive[i][1] + ".ws2" + archive[i][2] + ".ws2" + archive[i][3];
+						}
 					}
 				}
 				if (returnString.equals("")) {
@@ -35,21 +38,28 @@ public class CommandHandler extends Main{
 				}
 				
 			} else if (message[1].equals("say")) {
-				archiveAdd(usersList[id][1], message[2]);
-				repeat += "1:1";
+				if (checkIfNotNull(message[2])) {
+					archiveAdd(usersList[id][1], message[2]);
+					repeat += "1:1";
+				} else {
+					repeat += "1:0";
+				}
 				
 			}
 		} else {
 			if (message[1].equals("load")) {
-				if(DataRead.loadUser(message[0], false)) {
-					repeat += "3:1";
-					
-				} else if (DataSave.addData(message[0], message[2])) {
-					repeat += "3:1";
-					
+				if (checkIfNotNull(message[0])) {
+					if(DataRead.loadUser(message[0], false)) {
+						repeat += "3:1";
+						
+					} else if (DataSave.addData(message[0], message[2])) {
+						repeat += "3:1";
+						
+					} else {
+						repeat += "0:0";
+					}
 				} else {
 					repeat += "0:0";
-					
 				}
 			} else {
 				repeat += "3:0";
@@ -60,7 +70,7 @@ public class CommandHandler extends Main{
 	
 	public static boolean archiveAdd(String userName, String message) {
 		// Adding time.
-		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy-HH-mm-ss");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
 		Date date = new Date();
 		
 		// Move content of archive to farther position.
@@ -87,5 +97,19 @@ public class CommandHandler extends Main{
 		DataSave.archiveSave(userName, message);
 		
 		return true;
+	}
+	
+	public static boolean checkIfNumber(String string) {
+		if (string.replace("[0-9]+", "").length() == 0 && !string.equals("")) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean checkIfNotNull(String string) {
+		if (!string.equals("")) {
+			return true;
+		}
+		return false;
 	}
 }
